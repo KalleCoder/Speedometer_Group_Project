998cddac36eab8b4a0543d9471dd0deab2d444aa
KalleCoder <kallehandin@gmail.com>
2024-08-12 11:44:46 +0200

    added some small things

--- desktop/client/include/comservice.h
+++ desktop/client/include/comservice.h
@@ -31,7 +31,7 @@ public:
     bool get_status(); // Get the status of the communication
 
     virtual void run() = 0; // Receive the buffer over the communication protocol
-    virtual ~COMService() = 0;
+    virtual ~COMService() = default;
 };
 
 #endif // COMSERVICE_H
